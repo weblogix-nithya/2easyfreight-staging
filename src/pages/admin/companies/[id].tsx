99165fcba70147dc8629a7d8d656d697cf570737
sangeethaRamkumar <sangeetha.m@weblogix.com.au>
2025-04-02 16:13:36 +0530

    solidus ,Existing customer list expanded the limit

--- src/pages/admin/companies/[id].tsx
+++ src/pages/admin/companies/[id].tsx
@@ -210,8 +210,8 @@ function CompanyEdit() {
   } = useQuery(GET_CUSTOMERS_QUERY, {
     variables: {
       query: searchQuery,
-      page: queryPageIndex + 1,
-      first: queryPageSize,
+      page: 1,
+      first: 10000,
       orderByColumn: "id",
       orderByOrder: "ASC",
       company_id: null,
