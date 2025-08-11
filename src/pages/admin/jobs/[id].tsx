f8222eda54012ad99ab79c178fe2491a5d58ad6e
weblogix-nithya <nithya.c@weblogix.com.au>
2025-08-11 13:55:00 +0530

    KAN-109 - cbm caluclation changes updated.

--- src/pages/admin/jobs/[id].tsx
+++ src/pages/admin/jobs/[id].tsx
@@ -78,7 +78,7 @@ import {
 } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "store/store";
-
+import { calculateFinalWeightCBM } from "utils/calculatePalletSpacesOccupied";
 interface _CalculationData {
   cbm_auto: number;
   total_weight: number;
@@ -168,7 +168,7 @@ function JobEdit() {
 
   const [depotOptions, setDepotOptions] = useState([]);
   const [filtereddepotOptions, setFilteredDepotOptions] = useState([]);
-
+  const [companyWeight, setCompanyWeight] = useState(null);
   const [_selectedDepot, setSelectedDepot] = useState("");
 
   const [prevJobState, setPrevJobState] = useState({
@@ -367,8 +367,8 @@ function JobEdit() {
           data.job.pick_up_state == "Victoria"
             ? "VIC"
             : data.job.pick_up_state == "Queensland"
-            ? "QLD"
-            : "";
+              ? "QLD"
+              : "";
         const selectedLocation = locationOptions.find(
           (location) => location.label == data.job.pick_up_state,
         );
@@ -408,9 +408,9 @@ function JobEdit() {
         setIsSameDayJob(today === formatDate(data.job.ready_at));
         setIsTomorrowJob(
           new Date(formatDate(data.job.ready_at)).toDateString() ===
-            new Date(
-              new Date(today).setDate(new Date(today).getDate() + 1),
-            ).toDateString(),
+          new Date(
+            new Date(today).setDate(new Date(today).getDate() + 1),
+          ).toDateString(),
         );
 
         // ✅ Set destination data
@@ -439,22 +439,12 @@ function JobEdit() {
         );
 
         // ✅ Quote Calculation
-        const totalWeight =
-          data.job.job_items?.reduce(
-            (sum: any, item: { weight: any }) => sum + item.weight,
-            0,
-          ) || 0;
-
-        const totalCbm =
-          data.job.job_items?.reduce(
-            (sum: any, item: { volume: any }) => sum + item.volume,
-            0,
-          ) || 0;
+        const { totalCBM, totalWeight } = calculateFinalWeightCBM(job.job_category_id, jobItems, companyWeight);
 
         setQuoteCalculationRes((prev) => ({
           ...prev,
           total_weight: totalWeight,
-          cbm_auto: totalCbm,
+          cbm_auto: totalCBM,
         }));
       } else {
         // ✅ For updating media only
@@ -499,6 +489,10 @@ function JobEdit() {
       onCompleted: (data) => {
         if (data?.company == null) {
           // router.push("/admin/companies");
+        }
+        // console.log("Company data:", data.company);
+        if (data?.company?.weight_per_cubic != null) {
+          setCompanyWeight(data.company.weight_per_cubic);
         }
         setRefinedData({
           ...refinedData,
@@ -560,7 +554,7 @@ function JobEdit() {
     });
   };
 
-  const [handleUpdateJob, {}] = useMutation(UPDATE_JOB_MUTATION, {
+  const [handleUpdateJob, { }] = useMutation(UPDATE_JOB_MUTATION, {
     variables: {
       input: {
         id: job.id,
@@ -738,7 +732,7 @@ function JobEdit() {
     },
   });
 
-  const [handleDeleteJob, {}] = useMutation(DELETE_JOB_MUTATION, {
+  const [handleDeleteJob, { }] = useMutation(DELETE_JOB_MUTATION, {
     variables: {
       id: id,
     },
@@ -891,12 +885,11 @@ function JobEdit() {
         setButtonText("Update Quote");
       }
       //  setQuoteCalculationRes(defaultJobPriceCalculationDetail);
-      const totalWeight = jobItems.reduce((sum, item) => sum + item.weight, 0);
-      const totalCbm = jobItems.reduce((sum, item) => sum + item.volume, 0);
+      const { totalCBM, totalWeight } = calculateFinalWeightCBM(job.job_category_id, jobItems, companyWeight);
       setQuoteCalculationRes((prev) => ({
         ...prev,
         total_weight: totalWeight,
-        cbm_auto: totalCbm,
+        cbm_auto: totalCBM,
       }));
       getJob();
     },
@@ -906,12 +899,12 @@ function JobEdit() {
       setIsUpdateMode(false); // No data found, so we need to create a new entry
       setRefinedData(defaultJobQuoteData);
       setQuoteCalculationRes(defaultJobPriceCalculationDetail);
-      const totalWeight = jobItems.reduce((sum, item) => sum + item.weight, 0);
-      const totalCbm = jobItems.reduce((sum, item) => sum + item.volume, 0);
+
+      const { totalCBM, totalWeight } = calculateFinalWeightCBM(job.job_category_id, jobItems, companyWeight);
       setQuoteCalculationRes((prev) => ({
         ...prev,
         total_weight: totalWeight,
-        cbm_auto: totalCbm,
+        cbm_auto: totalCBM,
       }));
       setButtonText("Get A Quote");
       if (error.message.includes("No record found")) {
@@ -1014,23 +1007,30 @@ function JobEdit() {
     if (job?.company_id && job.company_id !== 0) {
       getCompanyRates({ variables: { company_id: Number(job.company_id) } });
     }
-  }, [job.company_id,getCompanyRates]);
+  }, [job.company_id, getCompanyRates]);
   useEffect(() => {
-    const totalWeight = jobItems.reduce((sum, item) => sum + item.weight, 0);
-    const totalCbm = jobItems.reduce((sum, item) => sum + item.volume, 0);
+    if (!jobItems || jobItems.length === 0) return; // no calculation if no items
 
-    setQuoteCalculationRes((prev) => ({
-      ...prev,
-      total_weight: totalWeight,
-      cbm_auto: totalCbm,
-    }));
-  }, [jobItems]);
+    const calculateTotals = () => {
 
-  const handleRemoveFromJobItems = (index: number) => {
+      const { totalCBM, totalWeight } = calculateFinalWeightCBM(job.job_category_id, jobItems, companyWeight);
+
+      setQuoteCalculationRes((prev) => ({
+        ...prev,
+        total_weight: totalWeight,
+        cbm_auto: totalCBM,
+      }));
+    };
+
+    calculateTotals();
+  }, [companyWeight, job.job_category_id, jobItems]);
+
+
+  function handleRemoveFromJobItems(index: number) {
     let _jobItems = [...jobItems];
     _jobItems.splice(index, 1);
     setJobItems(_jobItems);
-  };
+  }
   const handleJobItemChanged = (
     value: any,
     index: number,
@@ -1115,7 +1115,7 @@ function JobEdit() {
     //check if any job destination is_saved_address and populate setSavedAddresses
   };
   //handleDeleteJobItem
-  const [handleDeleteJobItem, {}] = useMutation(DELETE_JOB_ITEM_MUTATION, {
+  const [handleDeleteJobItem, { }] = useMutation(DELETE_JOB_ITEM_MUTATION, {
     onCompleted: (_data) => {
       // console.log("Job Item Deleted", data);
     },
@@ -1124,7 +1124,7 @@ function JobEdit() {
     },
   });
   //handleDelete
-  const [handleDeleteJobDestination, {}] = useMutation(
+  const [handleDeleteJobDestination, { }] = useMutation(
     DELETE_JOB_DESTINATION_MUTATION,
     {
       onCompleted: (_data) => {
@@ -1164,7 +1164,7 @@ function JobEdit() {
   };
   const [createJobDestination] = useMutation(CREATE_JOB_DESTINATION_MUTATION);
   //handleUpdateJobItems
-  const [handleUpdateJobItem, {}] = useMutation(UPDATE_JOB_ITEM_MUTATION, {
+  const [handleUpdateJobItem, { }] = useMutation(UPDATE_JOB_ITEM_MUTATION, {
     onCompleted: (_data) => {
       // console.log("Job item updated");
     },
@@ -1173,7 +1173,7 @@ function JobEdit() {
     },
   });
   //handleUpdateJobDestinations
-  const [handleUpdateJobDestination, {}] = useMutation(
+  const [handleUpdateJobDestination, { }] = useMutation(
     UPDATE_JOB_DESTINATION_MUTATION,
     {
       onCompleted: (_data) => {
@@ -1185,7 +1185,7 @@ function JobEdit() {
     },
   );
   //deleteMedia
-  const [handleDeleteMedia, {}] = useMutation(DELETE_MEDIA_MUTATION, {
+  const [handleDeleteMedia, { }] = useMutation(DELETE_MEDIA_MUTATION, {
     onCompleted: (_data) => {
       toast({
         title: "Attachment deleted",
@@ -1251,7 +1251,7 @@ function JobEdit() {
     },
     [re],
   );
-  const [handleCreateJobCcEmail, {}] = useMutation(
+  const [handleCreateJobCcEmail, { }] = useMutation(
     CREATE_JOB_CC_EMAIL_MUTATION,
     {
       variables: {
@@ -1308,13 +1308,13 @@ function JobEdit() {
     [jobCcEmailTags, jobCcEmails],
   );
 
-  const [handleDeleteJobCcEmail, {}] = useMutation(
+  const [handleDeleteJobCcEmail, { }] = useMutation(
     DELETE_JOB_CC_EMAIL_MUTATION,
     {
       variables: {
         id: deleteJobCcEmailId,
       },
-      onCompleted: (_data) => {},
+      onCompleted: (_data) => { },
       onError: (error) => {
         showGraphQLErrorToast(error);
       },
@@ -1427,11 +1427,11 @@ function JobEdit() {
     const jobDestination1 =
       jobDestinations.length > 0
         ? {
-            state: jobDestinations[0]?.address_state,
-            suburb: jobDestinations[0]?.address_city,
-            postcode: jobDestinations[0]?.address_postal_code,
-            address: jobDestinations[0]?.address,
-          }
+          state: jobDestinations[0]?.address_state,
+          suburb: jobDestinations[0]?.address_city,
+          postcode: jobDestinations[0]?.address_postal_code,
+          address: jobDestinations[0]?.address,
+        }
         : null;
 
     const selectedCategoryName = jobCategories.find(
@@ -1471,15 +1471,15 @@ function JobEdit() {
       state_code: refinedData.state_code || selectedstate?.value,
       company_rates:
         (job.job_category_id == 1 && selectedstate?.value === "QLD") ||
-        selectedstate?.value === "VIC"
+          selectedstate?.value === "VIC"
           ? filteredCompanyRates.map((rate) => ({
-              company_id: rate.company_id,
-              seafreight_id: rate.seafreight_id,
-              area: rate.area,
-              cbm_rate: rate.cbm_rate,
-              state: rate.state,
-              minimum_charge: rate.minimum_charge,
-            }))
+            company_id: rate.company_id,
+            seafreight_id: rate.seafreight_id,
+            area: rate.area,
+            cbm_rate: rate.cbm_rate,
+            state: rate.state,
+            minimum_charge: rate.minimum_charge,
+          }))
           : [],
       job_pickup_address: {
         state: pickUpDestination?.address_state,
@@ -1490,11 +1490,11 @@ function JobEdit() {
       job_destination_address:
         jobDestinations.length > 0
           ? {
-              state: jobDestinations[0]?.address_state,
-              suburb: jobDestinations[0]?.address_city,
-              postcode: jobDestinations[0]?.address_postal_code,
-              address: jobDestinations[0]?.address,
-            }
+            state: jobDestinations[0]?.address_state,
+            suburb: jobDestinations[0]?.address_city,
+            postcode: jobDestinations[0]?.address_postal_code,
+            address: jobDestinations[0]?.address,
+          }
           : {},
       pickup_time: {
         ready_by: readyAt,
