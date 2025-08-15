60556b66d0d9ddc7d8d9dfdc1284fa8c718abb90
sangeethaRamkumar <sangeetha.m@weblogix.com.au>
2025-08-16 00:55:51 +0530

    map issue fixed

--- next.config.js
+++ next.config.js
@@ -10,6 +10,11 @@ const nextConfig = {
         hostname: 's3.ap-southeast-2.amazonaws.com',
         pathname: '/2easy-prod-bucket/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'api.2easyfreight.com.au',
+        pathname: '/images/**',
+      },
     ],
   },
 };
