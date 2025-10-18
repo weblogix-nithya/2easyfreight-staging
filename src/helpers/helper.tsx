6ca8dd80301448dd44b1a6c3aa4a1d49414ea789
sangeethaRamkumar <sangeetha.m@weblogix.com.au>
2025-10-18 17:35:16 +0530

    client side call

--- src/helpers/helper.tsx
+++ src/helpers/helper.tsx
@@ -423,18 +423,14 @@ export function isSameDay(jobDate: string, timeZone: string): boolean {
 }
 
 // Get job destination timezone based on the latitude and longitude using Google Maps API
-// export async function getTimezone(lat: number, lng: number) {
-//   const response = await fetch(
-//     `https://maps.googleapis.com/maps/api/timezone/json?location=${lat},${lng}&timestamp=${moment().unix()}&key=${
-//       process.env.GOOGLE_API_KEY
-//     }`,
-//   );
-
-//   const data = await response.json();
-//   return data.timeZoneId;
-// }
 export async function getTimezone(lat: number, lng: number) {
-  const res = await fetch(`/api/google?type=timezone&lat=${lat}&lng=${lng}`);
-  const data = await res.json();
+  const response = await fetch(
+    `https://maps.googleapis.com/maps/api/timezone/json?location=${lat},${lng}&timestamp=${moment().unix()}&key=${
+      process.env.GOOGLE_API_KEY
+    }`,
+  );
+
+  const data = await response.json();
   return data.timeZoneId;
 }
+
